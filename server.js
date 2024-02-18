8a121cea28743e9ffa02a8eb924ab7e1ef351a99
Lambru Adrian Georgian <120751654+lambrugeorge@users.noreply.github.com>
2024-02-18 20:32:04 +0200

    server.js

--- server.js
+++ server.js
@@ -130,6 +130,9 @@ app.get('/profile/:id', (req, res) => {
   });
   
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
+
