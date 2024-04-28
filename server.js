403bb506f6bf9775b7d91dd74d5443e0b759a014
github_username <email_address>
2024-04-28 09:04:22 +0300

    postgre update

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const knex = require('knex');
 const db = knex({
   client: "pg",
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
-    user : 'postgres',
-    password : '1234',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
